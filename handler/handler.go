6694ecba7c759eb32d4c756e0cd6804baccfc0af
Arkanheal <arkanheal@hotmail.fr>
2023-02-20 09:58:05 +0100

    fix(handler): Meilleure gestion d'erreur
    Update du produit en cas de dupID et renvoi de l'erreur avant traitement

--- handler/handler.go
+++ handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"encoding/csv"
+	"log"
 	"regexp"
 	"strconv"
 	"strings"
@@ -13,81 +14,97 @@ import (
 )
 
 func parseOrderData(line string, regex string) string {
-    re := regexp.MustCompile(regex)
-    data := string(re.Find([]byte(line)))
-    return data
-}
-
-func returnErr(c *fiber.Ctx, err error, s int) {
-    panic(err)
-    c.Status(s).JSON(&fiber.Map{
-        "success": false,
-        "message": err,
-    })
+	re := regexp.MustCompile(regex)
+	data := string(re.Find([]byte(line)))
+	return data
 }
 
 func CreateOrder(c *fiber.Ctx) error {
-    var err error
+	var err error
 
-    order := new(model.Order)
+	order := new(model.Order)
 
-    // Get payload datas
-    data := string(c.Body()[:])
-    lines := strings.Split(strings.TrimSuffix(data, "\n"), "\n")
+	// Get payload datas
+	data := string(c.Body()[:])
+	lines := strings.Split(strings.TrimSuffix(data, "\n"), "\n")
 
-    // Get data into struct (kinda sad to do like this tbh)
-    order.Id, err = strconv.Atoi(parseOrderData(lines[0], `\d+`))
-    if err != nil {
-        returnErr(c, err, 400)
-    }
-    order.VAT, err = strconv. ParseFloat(parseOrderData(lines[1], `\d+\.\d*`), 64)
-    if err != nil {
-        returnErr(c, err, 400)
-    }
-    order.Price, err = strconv.ParseFloat(parseOrderData(lines[2], `\d+\.\d*`), 64)
-    if err != nil {
-        returnErr(c, err, 400)
-    }
+	// Get data into struct (kinda sad to do like this tbh)
+	order.Id, err = strconv.Atoi(parseOrderData(lines[0], `\d+`))
+	if err != nil {
+		log.Println(err)
+		return c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": err,
+		})
+	}
+	order.VAT, err = strconv.ParseFloat(parseOrderData(lines[1], `\d+\.\d*`), 64)
+	if err != nil {
+		log.Println(err)
+		return c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": err,
+		})
+	}
+	order.Price, err = strconv.ParseFloat(parseOrderData(lines[2], `\d+\.\d*`), 64)
+	if err != nil {
+		log.Println(err)
+		return c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": err,
+		})
+	}
 
-    // Parse product data in CSV
-    productsCSV := strings.Join(lines[4:], "\n")
-    reader := csv.NewReader(strings.NewReader(productsCSV))
+	// Parse product data in CSV
+	productsCSV := strings.Join(lines[4:], "\n")
+	reader := csv.NewReader(strings.NewReader(productsCSV))
 
-    field, err := reader.ReadAll()
-    if err != nil {
-        returnErr(c, err, 400)
-    }
+	field, err := reader.ReadAll()
+	if err != nil {
+		log.Println(err)
+		return c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": err,
+		})
+	}
 
-    // Insert order now (in case product wasn't valid)
-    _, err = database.DB.Query("INSERT INTO orders (id, VAT, total) VALUES ($1, $2, $3)", order.Id, order.VAT, order.Price)
-    if err != nil {
-        returnErr(c, err, 500)
-    }
+	// Insert order now (in case product wasn't valid)
+	_, err = database.DB.Query("INSERT INTO orders (id, VAT, total) VALUES ($1, $2, $3)", order.Id, order.VAT, order.Price)
+	if err != nil {
+		log.Println(err)
+		return c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": err,
+		})
+	}
 
-    // Insert product(s)
-    for _, product := range field[1:] {
-        _, err := database.DB.Query("INSERT INTO products (id, price, name) VALUES ($1, $2, $3)", product[1], product[2], product[0])
-        if err != nil {
-            returnErr(c, err, 500)
-        }
-        _, err = database.DB.Query("INSERT INTO product_order (product_id, order_id) VALUES ($1, $2)", product[1], order.Id)
-        if err != nil {
-            returnErr(c, err, 500)
-        }
-    }
+	// Insert product(s)
+	for _, product := range field[1:] {
+        // Imaginons qu'on change le prix du même produits
+		_, err := database.DB.Query(`INSERT INTO products (id, price, name)
+            VALUES ($1, $2, $3)
+            ON CONFLICT (id) DO UPDATE SET price = EXCLUDED.price, name = EXCLUDED.name`, product[1], product[2], product[0])
+		if err != nil {
+			log.Println(err)
+			return c.Status(500).JSON(&fiber.Map{
+				"success": false,
+				"message": err,
+			})
+		}
 
-    // Return order infos (STATUS 200) or 500 with err
-    if err := c.JSON(&fiber.Map{
-        "success": true,
-        "message": "Order and products successfully created",
-        /* "order_id": order.id */
-    }); err != nil {
-        c.Status(500).JSON(&fiber.Map{
-            "success": false,
-            "message": "Error creating order",
-        })
-    }
+        // Ajout pour le lien avec les commandes
+		_, err = database.DB.Query("INSERT INTO product_order (product_id, order_id) VALUES ($1, $2)", product[1], order.Id)
+		if err != nil {
+			return c.Status(500).JSON(&fiber.Map{
+				"success": false,
+				"message": err,
+			})
+		}
+	}
 
-    return c.SendString(data)
+	return c.JSON(&fiber.Map{
+		"success": true,
+		"message": "Order and products successfully created",
+		"order_id": order.Id,
+    })
 
 }
